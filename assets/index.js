090cdf6bc353643ce9876be59658f89605ffec26
erikwibowo <erikwibo@gmail.com>
2022-06-19 22:42:03 +0700

    initial commit

---
+++ assets/index.js
@@ -0,0 +1,24 @@
+// Floating Action Button
+const fab = document.querySelector(".back-to-top");
+fab.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+// Navbar Fixed
+window.onscroll = function () {
+  const nav = document.querySelector("nav");
+  const fixedNav = nav.offsetTop;
+  if (window.pageYOffset > fixedNav) {
+    nav.classList.add("navbar-fixed");
+    fab.style.display = "block";
+  } else {
+    nav.classList.remove("navbar-fixed");
+    fab.style.display = "none";
+  }
+};
+
+let menu = document.querySelector(".mobile-nav");
+let nav = document.querySelector(".nav");
+menu.addEventListener("click", function () {
+  nav.classList.toggle("menu-open");
+});
